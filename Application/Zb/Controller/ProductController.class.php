f51592ebdaeb134730c2c844ae52c17faf703462
LG <171736699@qq.com>
2017-02-20 23:56:23 +0800

    列表添加经纬度距离计算

--- Application/Zb/Controller/ProductController.class.php
+++ Application/Zb/Controller/ProductController.class.php
@@ -7,7 +7,7 @@ use Zb\Service\CommonService;
 class ProductController extends CommonController {
 
     //通用获取商品列表
-    private function getProductList($where)
+    private function getProductList($where,$km=null)
     {
         $productModel = M('zuban_product_goods','','DB_DSN');
         $sql = "SELECT count(c.`id`) AS `num` FROM `zuban_product_category` AS c INNER JOIN `zuban_product_goods` AS g ON c.`product_sys_code` = g.`product_sys_code` WHERE $where;";
@@ -18,7 +18,17 @@ class ProductController extends CommonController {
             return $this->pageAry;
         }
         $this->setPageRow();
-        $sql = "SELECT g.*, c.`category_id`, c.`category_name` FROM `zuban_product_category` AS c INNER JOIN `zuban_product_goods` AS g ON c.`product_sys_code` = g.`product_sys_code` WHERE $where LIMIT ".($this->page-1).",".$this->row.";";
+
+        $fied = "";
+        $order = " ORDER BY g.`update_time` DESC ";
+        if($km){
+            $lat = $km['latitude'];
+            $log = $km['logitude'];
+            $fied = ", round(6378.138*2*asin(sqrt(pow(sin( ($lat*pi()/180-g.`latitude`*pi()/180)/2),2)+cos($lat*pi()/180)*cos(g.`latitude`*pi()/180)* pow(sin( ($log*pi()/180-g.`logitude`*pi()/180)/2),2)))*1000) AS `juli`";
+            $order = " ORDER BY `juli` ASC, g.`update_time` DESC ";
+        }
+
+        $sql = "SELECT g.*, c.`category_id`, c.`category_name` $fied FROM `zuban_product_category` AS c INNER JOIN `zuban_product_goods` AS g ON c.`product_sys_code` = g.`product_sys_code` WHERE $where $order LIMIT ".($this->page-1).",".$this->row.";";
         $this->pageAry['list'] = $productModel->query($sql);
 
         return $this->pageAry;
@@ -29,7 +39,7 @@ class ProductController extends CommonController {
 		获取发布列表
 
 	*/
-    public function getShowProductList($categoryId=1)
+    public function getShowProductList($latitude,$logitude,$categoryId=1)
     {
     	$categoryId = intval($categoryId);
         if($categoryId < 0){
@@ -43,7 +53,8 @@ class ProductController extends CommonController {
         }
 
         $categoryIdListStr = join(",",$categoryIdList);
-        $this->pageAry = $this->getProductList("c.`category_id` IN ($categoryIdListStr)");
+
+        $this->pageAry = $this->getProductList("c.`category_id` IN ($categoryIdListStr)",array('latitude'=>$latitude,'logitude'=>$logitude));
         $this->pageAry['list'] = $this->getUserInfoByAryList($this->pageAry['list']);
 
         $this->returnSuccess($this->pageAry);
