1995f6a0bc0d19c39940e6a84cda765097f48521
何俊红 <704492613@qq.com>
2017-03-13 23:53:38 +0800

    地区接口

--- Application/Admin/Controller/RegionController.class.php
+++ Application/Admin/Controller/RegionController.class.php
@@ -7,15 +7,20 @@ class RegionController extends AdminCommonController {
 
 	/**
 
-		获取地区列表
+        获取地区列表
 
-	*/
-    public function getRegionList($code="1",$level=4,$mapping=null)
+    */
+    public function getRegionList($code="1",$level=4,$mapping=null,$fixAll=0)
     {
         if(strlen($code) < 0){
             $this->returnErrorNotice("地区编码错误！");
         }
         $regionList = $this->region_list($code,$level,$mapping);
+        if($fixAll > 0){
+            $push = $regionList[0];
+            unset($push['children']);
+            $regionList = $this->fixAllForTree($regionList,$push,$level-2,$mapping,"全国","name","code","parent_code");
+        }
         $this->returnSuccess($regionList);
     }
 }
