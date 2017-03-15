ebc3eea8dea614f900fee8c0255dddabc212a227
LG <171736699@qq.com>
2017-03-16 00:49:56 +0800

    用户列表

--- Application/Admin/Controller/UserController.class.php
+++ Application/Admin/Controller/UserController.class.php
@@ -220,68 +220,6 @@ class UserController extends AdminCommonController
      */
 
     /**
-     * 获取用户列表
-     * @param $token    用户标示
-     */
-    public function getUserList(){
-
-        $userBase = $this->checkToken();
-        $managerType = $userBase["manager_type"];
-        $region_code = $userBase["region_code"];
-
-        $whereArr = array();
-        if($isAll <= 0){
-            $whereArr["region_code"] = $region_code;
-        }
-
-        $userModel = M("zuban_user_base", '', "DB_DSN");
-        $this->pageAry["total"] = $userModel->where($whereArr)->count();
-        if($this->pageAry["total"] > 0){
-
-            $this->setPageRow();
-            $this->pageAry["list"] = $userModel->where($whereArr)->order("id DESC")->page($this->page, $this->row)->select();
-            $this->pageAry["list"] = $this->pageAry["list"] ? $this->pageAry["list"] : array();
-        }
-
-        return $this->returnSuccess($this->pageAry);
-    }
-
-    /**
-     * 获取用户信息
-     * @param $token    用户标示
-     */
-    public function getUserInfo(){
-
-        $keyAry = array(
-            'userId' => "用户标识不能为空"
-        );
-        //参数列
-        $parameters = $this->getPostparameters($keyAry);
-        if (!$parameters) {
-            $this->returnErrorNotice('请求失败!');
-        }
-
-        //获取自己的信息
-        $userBase = $this->checkToken();
-        $managerType = $userBase["manager_type"];
-        $region_code = $userBase["region_code"];
-
-        $userId = $parameters["userId"];
-        $whereArr = array();
-        if($isAll <= 0){
-            $whereArr["region_code"] = $region_code;
-        }
-        $whereArr["user_id"] = $userId;
-        $userModel = M("zuban_user_base", '', "DB_DSN");
-        $userInfo = $userModel->where($whereArr)->find();
-        if(!$userInfo){
-            $this->returnErrorNotice('该用户不存在!');
-        }
-
-        return $this->returnSuccess($userInfo);
-    }
-
-    /**
      * 新增用户列表
      * http://localhost/zuban_server/index.php?c=Admin&m=User&a=newUserCommonFilter&token=1111&pageSize=20&pageIndex=1&type=0&phone=15002164396&name=&startTime=&endTime=&source=
      * 请求方式:get
@@ -297,137 +235,34 @@ class UserController extends AdminCommonController
      * @param $region 区域code
      *
      */
-    public function newUserCommonFilter()
+    public function getUserList()
     {
-
         $keyAry = array(
-            'pageSize' => "",
-            'pageIndex' => "",
-            'startTime' => "",
-            'endTime' => "",
             'name' => "",
             'phone' => "",
-            'type' => "",
-            'region' => "",
+            'status' => "请选择用户状态",
         );
         //参数列
         $parameters = $this->getPostparameters($keyAry);
         if (!$parameters) {
             $this->returnErrorNotice('请求失败!');
         }
+        // $this->checkToken(1);
         $this->setPageRow();
-        $rs = array(
-            'list' => array(),
-            'total' => 0
-        );
-        $whereSql = " 1=1 ";
+        $whereSql = " `status`= {$parameters['status']} ";
         if(strlen($parameters['phone'])>0){
             $whereSql .= " AND `account`= '{$parameters['phone']}' ";
         }
         if(strlen($parameters['name'])>0){
             $whereSql .= " AND `nick_name`= '{$parameters['name']}'  ";
         }
-        if(strlen($parameters['region'])>0){
-            $whereSql .= " AND `region_code`= '{$parameters['region']}' ";
-        }
-        if(strlen($parameters['startTime'])>0){
-            $whereSql .= " AND `register_time`>= '{$parameters['startTime']}' ";
-        }else{
-            $nowTime=date('Y-m-d');
-            $whereSql .= " AND `register_time`>= '$nowTime' ";
-        }
-        if(strlen($parameters['endTime'])>0){
-            $whereSql .= " AND `register_time`<= '{$parameters['endTime']}' ";
-        }
         $userModel = M('zuban_user_base', '', 'DB_DSN');
-        $userCount = $userModel->where($whereSql)->count();
-        if ($userCount <= 0) {
-            $this->returnSuccess($rs);
+        $this->pageAry["total"] = intval($userModel->where($whereSql)->count());
+        if($this->pageAry["total"] > 0){
+            $this->pageAry["list"] = $userModel->where($whereSql)->order("`register_time` DESC ")->page($this->page, $this->row)->select();
         }
-        $userRs = $userModel->where($whereSql)->order("`register_time` DESC ")->page($this->page, $this->row)->select();
-        if (count($userRs) <= 0) {
-            $this->returnSuccess($rs);
-        }
-        $rs['list'] = $userRs;
-        $rs['total'] = $userCount;
-        $this->returnSuccess($rs);
-    }
 
-
-    /**
-     * 新增用户列表
-     * http://localhost/zuban_server/index.php?c=Admin&m=User&a=loginUserCommonFilter&token=1111&pageSize=20&pageIndex=1&type=0&phone=15002164396&name=&startTime=&endTime=&source=
-     * 请求方式:get
-     * 服务名:Wap
-     * 参数:
-     * @param $token 用户编号
-     * @param $pageIndex 页码
-     * @param $pageSize 页数
-     * @param $startTime 开始时间
-     * @param $endTime 结束时间
-     * @param $phone 手机号
-     * @param $name 姓名
-     * @param $region 区域code
-     *
-     */
-    public function loginUserCommonFilter()
-    {
-
-        $keyAry = array(
-            'pageSize' => "",
-            'pageIndex' => "",
-            'startTime' => "",
-            'endTime' => "",
-            'name' => "",
-            'phone' => "",
-            'region' => "",
-        );
-        //参数列
-        $parameters = $this->getPostparameters($keyAry);
-        if (!$parameters) {
-            $this->returnErrorNotice('请求失败!');
-        }
-        $this->setPageRow();
-        $rs = array(
-            'list' => array(),
-            'total' => 0
-        );
-        $whereSql='';
-        if(strlen($parameters['phone'])>0){
-            $whereSql .= " AND u.`account`= '{$parameters['phone']}' ";
-        }
-        if(strlen($parameters['name'])>0){
-            $whereSql .= " AND u.`nick_name`= '{$parameters['name']}'  ";
-        }
-        if(strlen($parameters['region'])>0){
-            $whereSql .= " AND u.`region_code`= '{$parameters['region']}' ";
-        }
-        if(strlen($parameters['startTime'])>0){
-            $whereSql .= " AND a.`update_time`>= '{$parameters['startTime']}' ";
-        }else{
-            $nowTime=date('Y-m-d');
-            $whereSql .= " AND a.`update_time`>= '$nowTime' ";
-        }
-        if(strlen($parameters['endTime'])>0){
-            $whereSql .= " AND a.`update_time`<= '{$parameters['endTime']}' ";
-        }
-        $userModel = M('zuban_user_base', '', 'DB_DSN');
-        $sqlCountStr = "SELECT a.`user_id` FROM `zuban_user_info` AS a  LEFT JOIN `zuban_user_base` AS u  ON u.`user_id` = a.`user_id` WHERE u.`status` =1 ".$whereSql;
-        $userCount = $userModel->query($sqlCountStr);
-        $userCount=count($userCount);
-        if ($userCount <= 0) {
-            $this->returnSuccess($rs);
-        }
-        $page=$this->page-1;
-        $num=$this->page*$this->row;
-        $sqlStr = "SELECT u.`user_id`, u.`nick_name`, u.`account`, u.`head_img`, u.`region_code`, u.`region_name`, u.`register_time`, a.`update_time` FROM `zuban_user_info` AS a  LEFT JOIN `zuban_user_base` AS u  ON u.`user_id` = a.`user_id` WHERE u.`status` =1 ".$whereSql." LIMIT $page,$num ";
-        $userRs = $userModel->query($sqlStr);
-        if (count($userRs) <= 0) {
-            $this->returnSuccess($rs);
-        }
-        $rs['list'] = $userRs;
-        $rs['total'] = $userCount;
-        $this->returnSuccess($rs);
+        $this->returnSuccess($this->pageAry);
     }
 
 
